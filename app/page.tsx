92f821711ec22e8aaa7876af4163ef2bb9c98c6e
Pond <pchanawin@gmail.com>
2024-11-03 20:48:36 +0700

    Installing ShadCN UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 const Home = () => {
-	return (
-		<>
-			<h1 className="h1-bold">Welcome to the world of Next.js</h1>
-			<h1 className="h1-bold font-space-grotesk">
-				Welcome to the world of Next.js (Inter)
-			</h1>
-		</>
-	);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    </>
+  );
 };
 
 export default Home;
