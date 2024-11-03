5b88afd0b2dc4fdcf4b6924cca5c7aaf49be25cb
Pond <pchanawin@gmail.com>
2024-11-03 14:28:22 +0700

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
 	return (
-		<h1 className="text-3xl text-violet-700">
+		<h1 className="text-3xl font-black text-violet-700">
 			Welcome to the world of Next.js
 		</h1>
 	);
